159e25c77414128888371a981d589085a42797f4
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2014-10-30 19:48:25 +0600

    Initial Commit

---
+++ ThreeSevenAce/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreeSevenAce
+//
+//  Created by neo on 30.10.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ThreeSevenAce/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ThreeSevenAce
+//
+//  Created by neo on 30.10.14.
+//  Copyright (c) 2014 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
